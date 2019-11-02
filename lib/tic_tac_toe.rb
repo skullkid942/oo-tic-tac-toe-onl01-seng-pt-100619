5dcc8d6897b2da628dc0c652e860adf512428a82
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 16:54:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,9 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  
+  def input_to_index(user_input)
+	  user_input.to_i - 1
+  end
 
   def move(location, character = "X")
     @board[location.to_i - 1] = character
