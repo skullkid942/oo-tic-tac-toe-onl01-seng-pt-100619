cb98082fb44a4c5c3f4111b984d8d84e86afe13f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 16:05:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,10 @@ class TicTacToe
   end 
   
   def turn_count
-    
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
+    end
   end 
 end 
