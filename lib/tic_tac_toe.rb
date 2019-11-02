9581f53d99ebdc73b7b7997017bc595c375b0566
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 16:09:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,5 +95,14 @@ def winner
 end
 end
 
-
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 end 
