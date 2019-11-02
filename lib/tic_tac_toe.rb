220290f18b9f999aa19ecc3d11325f8273512a8b
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 15:51:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,6 +6,14 @@ class TicTacToe
   end 
   
   def display_board
-    
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  def input_to_index
+    
+  end 
 end 
