bdb0876935e9fe1f2d27dc92a240380cd550b321
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 16:38:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
   ]
   
   
-    def display_board
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
